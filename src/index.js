7ace998eeb6ee809c2d6b0185b2e70516bfafabb
Babang <46173577+bagasysf@users.noreply.github.com>
2024-03-19 15:43:34 +0700

    Extracted Component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // Step 1: Import the React and React DOM Libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 // Step 2: Get a reference to the div with ID root
 const el = document.getElementById('root');
@@ -8,10 +9,5 @@ const el = document.getElementById('root');
 // Step 3: Tell React to take control of that element
 const root = ReactDOM.createRoot(el);
 
-// Step 4: Create a component
-function App() {
-  return <h1>Hi There!</h1>;
-}
-
-// Step 5: Show the component on the screen
+// Step 4: Show the component on the screen
 root.render(<App />);
